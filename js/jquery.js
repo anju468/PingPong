b254e98650b684f6b6a7e8d9e5ffd3f63289bec4
anju468 <anju468@gmail.com>
2015-10-18 17:58:36 -0700

    Update jquery.js

---
+++ js/jquery.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $(".pingpongOutput").hide();
+  $("#play").click(function(event) {
+    $(".pingpongOutput ul").empty();
+    var userInput = parseInt(prompt("How many numbers do you want to Ping-Pong?"));
+    for (var i=1; i<=userInput; i++) {
+      if ((i%3)===0 && (i%5)===0) {
+        $(".pingpongOutput ul").append("<li>Ping Pong</li>");
+      } else if ((i%3)===0 && (i%5)!==0) {
+          $(".pingpongOutput ul").append("<li>Ping</li>");
+        } else if ((i%3)!==0 && (i%5)===0) {
+            $(".pingpongOutput ul").append("<li>Pong</li>");
+          } else {
+              $(".pingpongOutput ul").append("<li>" + (i) + "</li>");
+            }
+    }
+    $(".pingpongOutput").show();
+    event.preventDefault();
+  });
+  $("#play-again").click(function(event) {
+    location.reload(true);
+    event.preventDefault();
+  });
+});
